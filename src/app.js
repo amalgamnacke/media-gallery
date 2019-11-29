c32f47bf42670c8c28eb86913904d2cf57127430
Gustav Lättman <gustav.lattman@gmail.com>
2019-11-29 16:13:49 +0100

    TEst.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 require('bootstrap');
-require('masonry-layout/dist/masonry.pkgd.min');
+//require('masonry-layout/dist/masonry.pkgd.min');
 require('lightgallery');
 require('lg-zoom');
 require('lg-video');
 require('lg-fullscreen');
 require('lg-hash');
+
+//require('isotope-layout');
+//require('isotope-packery');
+
+
+require("isotope-layout/dist/isotope.pkgd.min");
+require('isotope-packery/packery-mode');
+require('packery');
+require('imagesloaded');
+
 const eva = require('eva-icons');
 
-import './discord-data'
-import './scss/app.scss';
+require('./discord-data');
+require('./scss/app.scss');
 
 $(document).ready(function() {
   $("#lightgallery").lightGallery({
@@ -20,13 +30,19 @@ $(document).ready(function() {
     "enableSwipe": false
   });
 
-  /*$('.grid').masonry({
-    // set itemSelector so .grid-sizer is not used in layout
+  // init Packery
+  $('.grid').packery({
     itemSelector: '.grid-item',
-    // use element for option
-    columnWidth: '.grid-sizer',
+    gutter: 7,
     percentPosition: true
-  });*/
+  });
+  // layout Packery after each image loads
+  $(".grid").imagesLoaded().progress(function() {
+    console.log("image loaded!");
+    $(".grid").packery();
+
+
+  });
 });
 
 // Load the gallery.
--- src/discord-data.js
+++ src/discord-data.js
@@ -1,69 +1,76 @@
 import discordJson from './db.json';
+const packery = require('packery');
 
-var imageCount = getTotalNumberOfImages();
-var imagesPerColumn = imageCount <= 4 ? 1 : Math.ceil(imageCount / 4);
+function renderMedia(containerSelector, amount, offset) {
+  var renderedImageCount = 0;
 
-var container = $(`<div class="column"></div>`).appendTo("#lightgallery");
-var imageCountInCurrentColumn = 0;
-console.log(`Number of images ${imageCount}.`, `Number of images per column ${imagesPerColumn}.`);
-
-for (var messageId of Object.keys(discordJson)) {
-  var message = discordJson[messageId];
-
-  var urls = message["urls"];
-  if (!Array.isArray(urls)) {
-    continue;
-  }
-
-  for (var i = 0; i < urls.length; i++) {
-    if (imageCountInCurrentColumn >= imagesPerColumn) {
-      imageCountInCurrentColumn = 0;
-      container = $(`<div class="column"></div>`).insertAfter(container);
+  for (var messageId of Object.keys(discordJson).slice(offset)) {
+    if (renderedImageCount >= amount) {
+      return;
     }
 
-    var url = urls[i];
-
-    $(`
-      <a title="" href="${url.url}" class="grid-item" data-jump_url="${message["jump_url"]}" loading="lazy">
-        <img src="${url.img}" alt=""></img>
-
-        <div class="info">
-          <div class="message">${message["message"]}</div>
-          <div class="author">${message["author"]}</div>
-          <div class="date">${message["created_at"] ? message["created_at"] : ""}</div>
-        </div>
-
-        <i class="fas fa-search">
-      </a>
-    `).appendTo(container);
-
-    imageCountInCurrentColumn++;
-  }
-}
-
-function getTotalNumberOfImages() {
-  var imageCount = 0;
-
-  for (var messageId of Object.keys(discordJson)) {
     var message = discordJson[messageId];
 
     var urls = message["urls"];
     if (!Array.isArray(urls)) {
+      // Message contains no urls, should never happen.
       continue;
     }
 
     for (var i = 0; i < urls.length; i++) {
-      imageCount++;
+      if (renderedImageCount >= amount) {
+        return;
+      }
+
+      var url = urls[i];
+      var gridItem = $(`<a title="" href="${url.url}" class="grid-item " data-jump_url="${message["jump_url"]}">
+          <img src="${url.img}" alt=""></img>
+
+          <div class="info">
+            <div class="message">${message["message"]}</div>
+            <div class="author">${message["author"]}</div>
+            <div class="date">${message["created_at"] ? message["created_at"] : ""}</div>
+          </div>
+
+          <i class="fas fa-search">
+        </a>
+      `);
+
+      $(".grid").packery().append(gridItem).packery("appended", gridItem).packery();
+
+      renderedImageCount++;
     }
   }
-
-  return imageCount;
 }
 
+renderMedia("#lightgallery", 50, 0);
+
 $("#lightgallery").on('onBeforeSlide.lg',function(event, prevIndex, index, fromTouch, fromThumb) {
-  console.log(index);
   var currentSlide = $("#lightgallery .grid-item").eq(index);
   var jumpUrl = currentSlide.data("jump_url");
 
   $('.lg-toolbar').append(`<a target="_blank" class="lg-icon" href="${jumpUrl}"><i class="fab fa-discord"></i></a>`);
+});
+
+var cur = 51;
+$(window).scroll(function() {
+  var position = Math.round($(window).scrollTop());
+  var bottom = $(document).height() - $(window).height();
+
+  if((position + 500) >= bottom) {
+    renderMedia(".grid", 20, cur);
+
+    $(".grid").data("lightGallery").destroy(true);
+    $(".grid").lightGallery({
+      "selector": ".grid-item",
+      "counter": false,
+
+      "controls": false,
+      "enableDrag": false,
+      "enableSwipe": false
+    });
+    $(".grid").packery();
+
+    cur = cur + 20;
+  }
 });
